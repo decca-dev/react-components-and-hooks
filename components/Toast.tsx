ad8b324ba06b2d90ebc9040f44d9d0f636e6f0a1
decc00n <71793775+decca-dev@users.noreply.github.com>
2023-01-15 22:20:52 +0100

    Update Toast.tsx

--- components/Toast.tsx
+++ components/Toast.tsx
@@ -1,13 +1,13 @@
 import { useEffect, SetStateAction, Dispatch } from 'react';
 import Image from 'next/image';
 
-interface ToastOptions {
+export interface ToastOptions {
 	title: string;
 	description: string;
 	type: ToastType;
 }
 
-type ToastType = 'error' | 'success' | 'info' | 'loading';
+export type ToastType = 'error' | 'success' | 'info' | 'loading';
 
 const Toast = ({ title, description, type }: ToastOptions): JSX.Element => {
 	useEffect(() => {
@@ -18,37 +18,34 @@ const Toast = ({ title, description, type }: ToastOptions): JSX.Element => {
 	}, []);
 
 	return (
-		<div
-			id="toast"
-			className={'toast show toast-' + type}
-		>
-			<h6 className="inline-block align-middle">
-				<div className="inline-block align-middle mr-2">
-					<Image
-						src={'/assets/icons/' + type + '.svg'}
-						alt="icon"
-						width="25"
-						height="25"
-						className={type === 'loading' ? 'animate-spin' : ''}
-					/>
-				</div>
-				{title}
-			</h6>
-			<p>{description}</p>
+		<div className="flex flex-row items-center justify-center">
+			<div
+				id="toast"
+				className={'toast show toast-' + type}
+			>
+				<h6 className="inline-block align-middle">
+					<div className="inline-block align-middle mr-2">
+						<Image
+							src={'/assets/icons/' + type + '.svg'}
+							alt="icon"
+							width="25"
+							height="25"
+							className={type === 'loading' ? 'animate-spin' : ''}
+						/>
+					</div>
+					{title}
+				</h6>
+				<p>{description}</p>
+			</div>
 		</div>
 	);
 };
 
 export const clearMessage = (
-	setMessage: Dispatch<
-	SetStateAction<{
-		type: 'success' | 'error' | 'info';
-		text: string;
-	}>
-	>,
+	setMessage: Dispatch<SetStateAction<ToastOptions | undefined>>,
 ): void => {
 	setTimeout(() => {
-		setMessage({ type: 'success', text: '' });
+		setMessage({ type: 'success', title: '', description: '' });
 	}, 3000);
 };
 
