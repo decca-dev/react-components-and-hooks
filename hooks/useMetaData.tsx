256db6d4175155e99ec4f5b7b7098606f1af5edb
decc00n <71793775+decca-dev@users.noreply.github.com>
2023-07-26 14:49:23 +0100

    Update useMetaData.tsx

--- hooks/useMetaData.tsx
+++ hooks/useMetaData.tsx
@@ -1,39 +1,58 @@
+import { JSX } from 'react';
 import Head from 'next/head';
+
+const WEBSITE = 'name of website';
 
 export const useMetaData = (
 	title: string,
-	description: string,
 	url: string,
+	description = "default description",
 ): JSX.Element => {
 	return (
 		<Head>
-			<title>{`Aesculapia | ${title}`}</title>
+			<title>{`${WEBSITE} | ${title}`}</title>
+			<meta charSet='utf-8' />
+			<meta
+				name='viewport'
+				content='width=device-width, initial-scale=1'
+			/>
+			<meta
+				name='description'
+				content={description}
+			/>
 			<link
-				rel="shortcut icon"
-				href="/assets/logo.png"
-				type="image/png"
-			/>
-
-			<meta
-				property="og:title"
-				content={title}
+				rel='shortcut icon'
+				href='/assets/logo.png'
+				type='image/png'
 			/>
 			<meta
-				property="og:description"
+				property='og:title'
+				content={`${WEBSITE} | ${title}`}
+			/>
+			<meta
+				property='og:description'
 				content={description}
 			/>
 			<meta
-				property="og:url"
-				content={url}
+				property='og:type'
+				content='website'
 			/>
 			<meta
-				property="og:image"
-				content={`${process.env.NEXT_PUBLIC_DOMAIN}/api/og?description=${description}`}
+				property='og:url'
+				content={process.env.NEXT_PUBLIC_DOMAIN + url}
 			/>
 			<meta
-				content="#EF4444"
-				data-react-helmet="true"
-				name="theme-color"
+				property='og:image'
+				content='/assets/logo.png'
+			/>
+			<meta
+				name='keywords'
+				color='keyword1, keyword2'
+			/>
+			<meta
+				content='#dee610'
+				data-react-helmet='true'
+				name='theme-color'
 			/>
 		</Head>
 	);
